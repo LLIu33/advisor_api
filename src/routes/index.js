8f532ce6165b238784f3c57b6048cd3a1837629f
Artem Grechko <tuocs@yandex.ru>
2020-04-03 21:57:43 +0300

    Add simple documentation for API

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,74 @@ const swaggerUi = require('swagger-ui-express');
 const { createEntity, getEntity, getListOfEntity, updateEntity, delteEntity } = require('../controllers');
 const swaggerOptions = require('../utils/swagger');
 
-const rootRouter = express.Router(); // eslint-disable-line new-cap
+const rootRouter = express.Router();
 
 const apiRouter = express.Router();
+/**
+ * @swagger
+ * tags:
+ *   name: Entities
+ *   description: CRUD operations for entities
+ */
+
+/**
+ * @swagger
+ * /{collection}:
+ *   post:
+ *     description: Create entity
+ *     tags: [Entities]
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ */
 apiRouter.post('/:collection', createEntity);
+
+/**
+ * @swagger
+ * /{collection}:
+ *   get:
+ *     description: Get list of entities
+ *     tags: [Entities]
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ */
 apiRouter.get('/:collection', getListOfEntity);
+
+/**
+ * @swagger
+ * /{collection}/{item_id}:
+ *   get:
+ *     description: Get entity
+ *     tags: [Entities]
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ */
 apiRouter.get('/:collection/:item_id', getEntity);
+
+/**
+ * @swagger
+ * /{collection}/{item_id}:
+ *   put:
+ *     description: Update entity
+ *     tags: [Entities]
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ */
 apiRouter.put('/:collection/:item_id', updateEntity);
+
+/**
+ * @swagger
+ * /{collection}/{item_id}:
+ *   delete:
+ *     description: Delete entity
+ *     tags: [Entities]
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ */
 apiRouter.delete('/:collection/:item_id', delteEntity);
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -12,7 +12,7 @@ const swaggerSpec = {
       },
     },
   },
-  apis: ['../routes'],
+  apis: ['./src/routes/*.js'],
   host,
   basePath: '/',
 };
