83e725e25c51ac6ee25e6518cd1fb38916ea84a1
Artem Grechko <tuocs@yandex.ru>
2020-04-08 15:39:32 +0300

    Add CRUD for lists

---
+++ src/controllers/list.js
@@ -0,0 +1,68 @@
+const listModel = require('../models/list');
+const helper = require('./helper');
+
+const getList = async (req, res) => {
+  try {
+    const { limit, offset, ...filterParams } = req.query;
+    const params = {
+      limit: helper.processLimit(limit),
+      offset: helper.processOffset(offset),
+      filter: helper.processFilter(filterParams),
+    };
+
+    const response = await listModel.getListOfLists(params);
+    return res.status(200).send(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+};
+const get = async (req, res) => {
+  try {
+    const entityId = req.params.item_id;
+    const response = await listModel.getById(entityId);
+    return res.status(200).send(response);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+};
+const create = async (req, res) => {
+  try {
+    const newData = req.body;
+    await listModel.create(newData);
+    return res.status(200).send();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+};
+const update = async (req, res) => {
+  try {
+    const entityId = req.params.item_id;
+    const newData = req.body;
+    await listModel.updateById(entityId, newData);
+    return res.status(200).send();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+};
+const remove = async (req, res) => {
+  try {
+    const entityId = req.params.item_id;
+    await listModel.deleteById(entityId);
+    return res.status(200).send();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+};
+
+module.exports = {
+  getList,
+  get,
+  create,
+  update,
+  remove,
+};
---
+++ src/routes/list.js
@@ -0,0 +1,84 @@
+const express = require('express');
+
+const ListCtrl = require('../controllers/list');
+const queryValidation = require('../middlewares/queryValidation');
+
+const listRouter = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Lists
+ *   description: CRUD operations for lists
+ */
+
+/**
+ * @swagger
+ * /api/lists:
+ *   get:
+ *     description: Get list of lists
+ *     tags: [Lists]
+ *     parameters:
+ *       - $ref: '#/components/parameters/Offset'
+ *       - $ref: '#/components/parameters/Limit'
+ *     responses:
+ *       200:
+ *         description: Successfully got list of lists
+ */
+listRouter.get('/lists', queryValidation, ListCtrl.getList);
+
+/**
+ * @swagger
+ * /api/lists:
+ *   post:
+ *     description: Create list
+ *     tags: [Lists]
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ */
+listRouter.post('/lists', queryValidation, ListCtrl.create);
+
+/**
+ * @swagger
+ * /api/lists/{item_id}:
+ *   get:
+ *     parameters:
+ *       - $ref: '#/components/parameters/EntityId'
+ *     description: Get list
+ *     tags: [Lists]
+ *     responses:
+ *       200:
+ *         description: Successfully retuned
+ */
+listRouter.get('/lists/:item_id', queryValidation, ListCtrl.get);
+
+/**
+ * @swagger
+ * /api/lists/{item_id}:
+ *   put:
+ *     parameters:
+ *       - $ref: '#/components/parameters/EntityId'
+ *     description: Update list
+ *     tags: [Lists]
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
+listRouter.put('/lists/:item_id', queryValidation, ListCtrl.update);
+
+/**
+ * @swagger
+ * /api/lists/{item_id}:
+ *   delete:
+ *     parameters:
+ *       - $ref: '#/components/parameters/EntityId'
+ *     description: Delete list
+ *     tags: [Lists]
+ *     responses:
+ *       200:
+ *         description: Successfully removed
+ */
+listRouter.delete('/lists/:item_id', queryValidation, ListCtrl.remove);
+
+module.exports = listRouter;
