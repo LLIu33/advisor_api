6981a302ea75a96d4dbcd604fcc8b552594b07f7
Artem Grechko <tuocs@yandex.ru>
2020-04-16 02:58:52 +0300

    Add seed for reviews

---
+++ src/seeders/20200415182800-reviews-seed.js
@@ -0,0 +1,70 @@
+const helper = require('../utils/helper');
+const fs = require('fs');
+
+module.exports = {
+  up: async (queryInterface) => {
+    const data = require('../../data/places.json')['collection:places'];
+    const dataToInsert = [];
+    for (const key in data) {
+      const entity = data[key];
+      if (entity['collection:reviews']) {
+        const reviews = entity['collection:reviews'];
+        for (const reviewKey in reviews) {
+          let profile = [];
+          let place = [];
+          const placeQuery = `SELECT id from Places WHERE uid = "${entity.id}";`;
+          place = await queryInterface.sequelize.query(placeQuery, {
+            type: queryInterface.sequelize.QueryTypes.SELECT,
+          });
+          const review = reviews[reviewKey];
+          // console.log(review);
+          if (review.userID) {
+            const profileQuery = `SELECT id from Profiles WHERE uid = "${review.userID}";`;
+            profile = await queryInterface.sequelize.query(profileQuery, {
+              type: queryInterface.sequelize.QueryTypes.SELECT,
+            });
+          }
+          const atmosphereRating = helper.emptyOrNullToString(review.rating.atmosphere);
+          const serviceRating = helper.emptyOrNullToString(review.rating.service);
+          const qualityRating = helper.emptyOrNullToString(review.rating.quality);
+          const item = {
+            uid: review.id,
+            userId: profile.length ? profile[0].id : null,
+            userUid: review.userID,
+            placeId: place.length ? place[0].id : null,
+            placeUid: entity.id,
+            atmosphereRating: atmosphereRating.data,
+            serviceRating: serviceRating.data,
+            qualityRating: qualityRating.data,
+            atmosphereText: review.ratingTexts.atmosphere,
+            serviceText: review.ratingTexts.service,
+            qualityText: review.ratingTexts.quality,
+            text: review.text,
+            publishedAt: helper.fbTimestampToDatetime(review.date),
+          };
+          // console.log(item);
+          dataToInsert.push(item);
+        }
+      }
+    }
+    console.log(dataToInsert);
+    // fs.writeFileSync('./reviews.json', JSON.stringify(dataToInsert), 'utf8');
+    console.log('dataToInsert.length: ', dataToInsert.length);
+    return queryInterface.bulkInsert('Reviews', dataToInsert, {
+      timestamps: true,
+      charset: 'utf8',
+      collate: 'utf8mb4_general_ci',
+    });
+  },
+
+  down: (queryInterface) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Reviews', null, {});
+  },
+};
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,18 +1,38 @@
 const moment = require('moment');
 
 const fbTimestampToDatetime = (input) => {
+  if (!input || !input._seconds) {
+    return null;
+  }
+  if (typeof input._seconds === 'string') {
+    input._seconds = getDataFromJson(input._seconds);
+  }
   const timestamp = input && input._seconds ? input._seconds.data : null;
   return timestamp ? moment.unix(timestamp).format('YYYY-MM-DD hh:mm:ss') : null;
 };
 
 const emptyOrNullToString = (input) => {
-  if (!input || (typeof input === 'object' && input.type === 'null')) {
+  if (typeof input === 'string') {
+    input = getDataFromJson(input);
+  }
+  if (!input || input.type === 'null') {
     return '';
   }
   return input;
 };
 
+const getDataFromJson = (input) => {
+  try {
+    const data = JSON.parse(input);
+    return data;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
 module.exports = {
   fbTimestampToDatetime,
   emptyOrNullToString,
+  getDataFromJson,
 };
